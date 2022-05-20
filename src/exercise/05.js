076fb7fe5469d7b6ca6d120b571d80f2bdf733c5
nicmlu <NicoleMLucien@gmail.com>
2022-05-19 23:21:32 -0400

    Exercise 5 EC 2 Solution accept a size prop to encapsulate styling

--- src/exercise/05.js
+++ src/exercise/05.js
@@ -14,10 +14,11 @@ import {italic} from 'ansi-colors'
 // matches what the text says it should be
 // 🐨 also use the style prop to make the font italic
 // 💰 Here are available style attributes: backgroundColor, fontStyle
-function Box({className = '', style, ...otherProps}) {
+function Box({size, style, ...otherProps}) {
+  const sizeClassName = size ? `box--${size}` : ``
   return (
     <div
-      className={`box ${className}`}
+      className={`box ${sizeClassName}`}
       style={{fontStyle: 'italic', ...style}}
       {...otherProps}
     />
@@ -25,19 +26,19 @@ function Box({className = '', style, ...otherProps}) {
 }
 
 const smallBox = (
-  <Box className="box--small" style={{backgroundColor: 'lightblue'}}>
+  <Box size="small" style={{backgroundColor: 'lightblue'}}>
     small lightblue box
   </Box>
 )
 
 const mediumBox = (
-  <Box className="box--medium" style={{backgroundColor: 'pink'}}>
+  <Box size="medium" style={{backgroundColor: 'pink'}}>
     medium pink box
   </Box>
 )
 
 const largeBox = (
-  <Box className="box--large" style={{backgroundColor: 'orange'}}>
+  <Box size="large" style={{backgroundColor: 'orange'}}>
     large orange box
   </Box>
 )
